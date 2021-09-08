b967382fcacf154b35629c460b8b92097c67454c
DrumPire <expresproject@gmail.com>
2021-09-08 12:42:58 +0300

    create open/close modal

---
+++ script/script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const btnOpenModal = document.querySelector('#btnOpenModal'),
+        modalBlock = document.querySelector('#modalBlock'),
+        btnCloseModal = document.querySelector('#closeModal'),
+        questionTitle = document.querySelector('#question'),
+        formAnswers = document.querySelector('#formAnswers');
+
+  const playTest = () => {
+    const renderQuestions = () => {
+      questionTitle.textContent = 'Какого цвета бургер вы хотите?';
+      formAnswers.innerHTML = `
+        <div class="answers-item d-flex flex-column">
+          <input type="radio" id="answerItem1" name="answer" class="d-none">
+          <label for="answerItem1" class="d-flex flex-column justify-content-between">
+            <img class="answerImg" src="./image/burger.png" alt="burger">
+            <span>Стандарт</span>
+          </label>
+        </div>
+      `;
+    };
+    renderQuestions();
+  };
+
+  const openModal = () => {
+    modalBlock.classList.add('d-block');
+    playTest();
+  };
+
+  const closeModal = () => modalBlock.classList.remove('d-block');
+
+  btnOpenModal.addEventListener('click', openModal);
+  btnCloseModal.addEventListener('click', closeModal);
+});
+
